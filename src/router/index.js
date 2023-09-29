d9dbea3832a370b268dee43d4a9bfb222b29eecb
Yorusito <sebastian1987102@gmail.com>
2023-09-29 02:37:40 -0500

    feat(appointment): index.js update

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeComponent from "../public/pages/home.component.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
-      component: HomeView
+      component: HomeComponent
+    },
+    {
+      path: '/',
+      redirect: 'home'
     },
     {
       path: '/about',
@@ -15,8 +19,24 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: () => import('../public/pages/about.component.vue')
+    },
+    {
+      path: "/tutorials",
+      name: "tutorials",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../appointment/pages/appointment.component.vue"),
+    },
+    {
+      path: '/faq',
+      name: 'faq',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../public/pages/faq.component.vue')
+    },
   ]
 })
 
