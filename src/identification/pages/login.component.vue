3b1c219746e4d97e39397f16bbf8818de4a7131a
JulianaYS <u20201b782@upc.edu.pe>
2023-10-24 18:12:20 -0500

    feat(login): added style login component.

--- src/identification/pages/login.component.vue
+++ src/identification/pages/login.component.vue
@@ -12,7 +12,7 @@
       <pv-card class=" border-transparent ">
 
         <join />
-        <template #title><h2 class="title with-margin" style="margin-top: 2cm;">Log in</h2></template>
+        <template #title><h2 class="title with-margin" style="margin-top: 0cm;">Log in</h2></template>
         <template #content>
 
           <div class="form grid with-margin">
@@ -26,15 +26,12 @@
               <input id="Password" type="text" v-model="password" required class="border border-transparent rounded p-3  w-full text-lg text-decoration underline" placeholder="Password">
               <label for="Password" style="margin-left: 0.5cm; color: dimgray;" > Forgot your password?</label>
             </div>
-          </div>31115
-
-
-
+          </div>
 
           <div class="form grid with-margin">
             <div class="field col flex justify-content-center">
               <pv-button @click.prevent="submitForm()" type="button"
-                         class=" btn-google text-black text-base border-1 border-solid  border-round cursor-pointer transition-all transition-duration-200 hover:bg-primary-600 hover:border-primary-600 active:bg-primary-700 active:border-primary-700"
+                         class=" btn-google text-black text-base border-1 border-solid  border-round cursor-pointer transition-all transition-duration-200 hover:bg-600 hover:border-600 active:bg-700 active:border-700"
                          style="font-size: 1rem; padding: 10px 30px; border-radius: 15px; background: #ffffff; color: black;"> <i class="fab fa-google" style="margin-right: 10px;"></i> Continue with Google</pv-button>
             </div>
           </div>
@@ -42,7 +39,7 @@
           <div class="form grid with-margin">
             <div class="field col flex justify-content-center">
               <pv-button @click.prevent="submitForm()" type="button"
-                         class=" btn-facebook text-white text-base border-1 border-solid  border-round cursor-pointer transition-all transition-duration-200 hover:bg-primary-600 hover:border-primary-600 active:bg-primary-700 active:border-primary-700"
+                         class=" btn-facebook text-white text-base border-1 border-solid  border-round cursor-pointer transition-all transition-duration-200 "
                          style="  font-size: 1rem; padding: 10px 20px; border-radius: 15px; background: #2196F3; "> <i class="fab fa-facebook" style="margin-right: 10px;"></i> Continue with Facebook</pv-button>
             </div>
           </div>
@@ -50,8 +47,8 @@
           <div class="form grid with-margin">
             <div class="field col flex justify-content-center">
               <pv-button @click.prevent="submitForm()" type="button"
-                         class=" btn-login text-white text-base border-1 border-solid  border-round cursor-pointer transition-all transition-duration-200 hover:bg-primary-600 hover:border-primary-600 active:bg-primary-700 active:border-primary-700"
-                         style="font-size: 1.5rem; padding: 15px 30px; border-radius: 15px;">Login</pv-button>
+                         class=" btn-login text-white text-base border-1 border-solid  border-round cursor-pointer transition-all transition-duration-200 "
+                         style="font-size: 1.5rem; padding: 15px 30px; border-radius: 15px; background: #31b4a7;">Login</pv-button>
             </div>
           </div>
 
@@ -64,5 +61,31 @@
 </template>
 
 <style scoped>
+
+.container{
+  border-radius: 5px;
+  position: absolute;
+  padding: 0px;
+  margin: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top:19%;
+  right: 0px;
+  width: 800px;
+  height: 600px;
+  border-color: transparent;
+}
+
+.title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+}
+
+.with-margin {
+  margin-bottom: 0.5cm; /* Agrega un margen inferior de 3 cm */
+}
 
 </style>
