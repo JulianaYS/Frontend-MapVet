affd049ae89c72d2b7aaa81b35114d489b2f2c60
daniel <u202121440@upc.edu.pe>
2023-10-24 02:01:53 -0500

    feat(store): added toolbar store component

--- src/store/pages/store.component.vue
+++ src/store/pages/store.component.vue
@@ -1,11 +1,26 @@
 <script setup>
+import {ref} from 'vue';
+const visibleRight = ref(false);
+</script>
+
+<script>
 
 </script>
 
 <template>
+  <div class="card">
+    <pv-toolbar style="background: #fff0cb;">
+      <template #start>
+        <pv-button class="mr-2" label="Our products" style="background: #b4eee8; color:black;"/>
+      </template>
 
+      <template #end>
+        <pv-button class="mr-2" icon="pi pi-shopping-cart" :label="'Cart (' + cart.length + ')'" style="background: #c39777; color:black;" @click="visibleRight = true"/>
+      </template>
+    </pv-toolbar>
+  </div>
 </template>
 
-<style scoped>
+<style>
 
 </style>
