d0a4594bb308b6c60c84ca5e6cca4c567fd2dab7
JulianaYS <u20201b782@upc.edu.pe>
2023-11-15 04:23:45 -0500

    feat(profile): created and added profile component

---
+++ src/profile/pages/profile.component.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="profile-container">
+    <h1>Profile</h1>
+    <div class="profile-info">
+      <img :src="user.photo" alt="Profile Photo" class="profile-photo" />
+      <div class="attributes">
+        <p>Name: </p>
+        <p>Email: </p>
+        <p>Gender: </p>
+        <p>Birthdate: </p>
+      </div>
+      <div class="data">
+        <div class="editable-data">
+          <p>{{ user.name }}</p>
+          <i class="fas fa-pencil"></i>
+        </div>
+        <div class="editable-data">
+          <p>{{ user.email }}</p>
+          <i class="fas fa-pencil"></i>
+        </div>
+        <div class="editable-data">
+          <p>{{ user.gender }}</p>
+          <i class="fas fa-pencil"></i>
+        </div>
+        <div class="editable-data">
+          <p>{{ user.birthDate }}</p>
+          <i class="fas fa-pencil"></i>
+        </div>
+      </div>
+    </div>
+    <router-link to="/petprofile">
+      <button class="view-pets-button" style="font-size: 1rem; padding: 15px 30px;">See pets</button>
+    </router-link>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
