4f4856a2091ee92806c6791a2feb1134ae23781b
JulianaYS <u20201b782@upc.edu.pe>
2023-11-15 04:27:29 -0500

    feat(profile): added style profile component.

--- src/profile/pages/profile.component.vue
+++ src/profile/pages/profile.component.vue
@@ -41,4 +41,87 @@
 
 <style scoped>
 
+.profile-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 87vh;
+  background: rgba(255, 240, 203, 1);
+}
+
+.profile-info {
+  display: flex;
+  align-items: center;
+}
+
+.profile-photo {
+  width: 300px; height: 300px;
+  border-radius: 50%;
+  margin-right: 50px;
+}
+.editable-data {
+  display: flex;
+  align-items: baseline;
+}
+
+.fa-pencil {
+  cursor: pointer;
+}
+
+.data,
+.attributes {
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.data p{
+  font-family: 'Inria Sans', sans-serif;
+  background: white;
+  border-radius: 10px;
+  width: 350px;
+  height: 30px;
+  font-weight: bold;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  margin-bottom: 60px;
+}
+
+.attributes p{
+  font-family: 'Inria Sans', sans-serif;
+  justify-content: center;
+  margin-bottom: 60px;
+}
+
+.view-pets-button {
+  font-family: 'Inria Sans', sans-serif;
+  padding: 9px;
+  background-color: #31b4a7;
+  color: #ffffff;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-weight: bold;
+}
+h1 {
+  font-family: 'Inria Sans', sans-serif;
+  font-size: 3em;
+  text-align: center;
+}
+
+@media screen and (max-width: 600px) {
+  .profile-info {
+    flex-direction: column;
+    text-align: center;
+  }
+
+  .attributes p,
+  .data p {
+    width: auto;
+  }
+}
+
 </style>
